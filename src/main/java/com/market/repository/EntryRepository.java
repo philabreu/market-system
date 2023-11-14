c1e12d635a1c8be1bbb37785bcc598eb6b81bcd2
Filipe Abreu Bezerra <>
2023-11-14 16:42:54 -0300

    cache pra metodo findByDate.

--- src/main/java/com/market/repository/EntryRepository.java
+++ src/main/java/com/market/repository/EntryRepository.java
@@ -1,6 +1,7 @@
 package com.market.repository;
 
 import com.market.model.Entry;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,11 @@ import java.util.List;
 
 @Repository
 public interface EntryRepository extends JpaRepository<Entry, Long> {
+    /**
+     * Cache foi usado para requisito de 500 requisicoes por segundo.
+     * @param entryDate
+     * @return
+     */
+    @Cacheable("entries")
     List<Entry> findAllByEntryDate(LocalDate entryDate);
 }
